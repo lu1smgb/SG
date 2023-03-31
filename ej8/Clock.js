506552f98ff3ccf3d0cdef1015e7124d6d5c7708
lu1smgb <lu1smgb.gacc@gmail.com>
2023-03-31 11:04:52 +0100

    ej8 pendiente

--- ej8/Clock.js
+++ ej8/Clock.js
@@ -5,39 +5,56 @@ class Clock extends THREE.Object3D {
 
         super();
 
-        var geomIndicador = new THREE.SphereGeometry(1,10,10);
+        var geomIndicador = new THREE.SphereGeometry(0.5,30,30);
+        var geomManecilla = new THREE.IcosahedronGeometry(1,0);
         var matIndicador = new THREE.MeshPhongMaterial({ color: 0x00aaff });
         var matManecilla = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 
-        var distanciaIndicadores = 6;
-        var distanciaManecilla = 4;
+        this.clk = new THREE.Clock();
+
+        this.distanciaIndicadores = 6;
+        this.distanciaManecilla = 4;
+
+        this.numMarca = 0;
 
         for (let i=0; i < 12; i++) {
             var indicador = new THREE.Mesh(geomIndicador, matIndicador);
-            indicador.position.set(Math.cos(i * (2 * Math.PI / 12)) * distanciaIndicadores,
+            indicador.position.set(Math.cos(i * (2 * Math.PI / 12)) * this.distanciaIndicadores,
                                    0,
-                                   Math.sin(i * (2 * Math.PI / 12)) * distanciaIndicadores);
+                                   Math.sin(i * (2 * Math.PI / 12)) * this.distanciaIndicadores);
             if (i == 0) {
-                var manecilla = new THREE.Mesh(geomIndicador, matManecilla);
-                manecilla.position.set(distanciaManecilla, 0, 0);
+                var manecilla = new THREE.Mesh(geomManecilla, matManecilla);
+                //manecilla.position.set(this.distanciaManecilla, 0, 0);
                 manecilla.name = 'manecilla';
                 this.add(manecilla);
             }
             this.add(indicador);
         }
 
+        this.createGui(gui,titleGui);
+
     }
 
     createGui(gui, titleGui) {
 
-        
+        this.guiControls = {
+            mps: 1,
+
+            reset: () => {
+                this.guiControls.mps = 1;
+            }
+        }
+
+        var folder = gui.addFolder('Reloj');
+
+        folder.add(this.guiControls, 'mps',-12,12,1).name('Marcas por segundo').listen();
 
     }
 
     update(gui, titleGui) {
 
-        
-        
+        // REHACER POR COMPLETO (VER TEMA DE TEORIA DE ANIMACION)
+
     }
 }
 export { Clock }
--- ej8/MyScene.js
+++ ej8/MyScene.js
@@ -78,7 +78,7 @@ class MyScene extends THREE.Scene {
     //   Los planos de recorte cercano y lejano
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     // También se indica dónde se coloca
-    this.camera.position.set (20, 10, 20);
+    this.camera.position.set (10, 30, 10);
     // Y hacia dónde mira
     var look = new THREE.Vector3 (0,0,0);
     this.camera.lookAt(look);
